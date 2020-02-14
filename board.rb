4b5f5a519955912a4565f5bb0ee4c34efa32b96f
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-13 21:10:13 -0500

    Set up framework for successfully instantiating a Board instance

--- board.rb
+++ board.rb
@@ -1,2 +1,14 @@
+require_relative "piece"
+
+
 class Board
+    def initialize
+        @grid = populate_board
+    end
+
+    private
+
+    def populate_board
+        # populate a default 8x8 chess board grid
+    end
 end
