404e72d32f59f97ac7b9518fc8bb580c8e8e6636
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-13 21:21:05 -0500

    Create an empty Piece class with placeholder attributes, like the type of piece Create a NullPiece class that extends the Piece class

--- piece.rb
+++ piece.rb
@@ -1,2 +1,16 @@
 class Piece
+    attr_reader :type
+    def initialize(type)
+        @type = type
+    end
+
+    def inspect
+        @type.inspect
+    end
+end
+
+class NullPiece < Piece
+    def initialize
+        super("NullPiece")
+    end
 end
