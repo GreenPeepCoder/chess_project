79ea1508620b690870d70038550b556a867181e2
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-15 18:25:37 -0500

    Outline methods the Pawn class is going to need

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -5,4 +5,22 @@ class Pawn < Piece
     def symbol
         '♟'.colorize(color)
     end
+
+    def move_dirs
+    end
+
+    private 
+
+    def at_start_row?
+    end
+
+    def forward_dir
+        # returns 1 or -1
+    end
+
+    def forward_steps
+    end
+
+    def side_attacks
+    end
 end
