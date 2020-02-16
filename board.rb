1f624c9fb9f8f4f6c6df06429737f957b1076391
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-16 14:44:52 -0500

    Update private #populate_board method, along with its helper methods

--- board.rb
+++ board.rb
@@ -7,7 +7,7 @@ class Board
 
     def initialize(fill_board = true)
         @sentinel = NullPiece.instance
-        populate_board
+        populate_board(fill_board)
     end
 
     def [](pos)
@@ -49,8 +49,31 @@ class Board
     private
     attr_reader :sentinel
     
-    def populate_board
+    def fill_back_row(color)
+        back_pieces = [
+            Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
+        ]
+
+        i = color == :white ? 7 : 0
+        back_pieces.each_with_index do |piece_class, j|
+            piece_class.new(color, self, [i, j])
+        end
+    end
+
+    def fill_pawn_row(color)
+        i = color == :white ? 6 : 1
+        8.times { |j| Pawn.new(color, self, [i,j])}
+    end
+
+    def populate_board(fill_board)
         # populate a default 8x8 chess board grid
         @rows = Array.new(8){Array.new(8, sentinel)}
+        return unless fill_board
+        possible_colors = %i(white black)
+        possible_colors.each do |color|
+            fill_back_row(color)
+            fill_pawn_row(color)
+        end
     end
+
 end
