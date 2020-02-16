395ebd37bec71db1adc3b2604c7a3b28648be802
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-16 16:26:05 -0500

    Add forward step functionality to Pawn class

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -6,19 +6,30 @@ class Pawn < Piece
         '♟'.colorize(color)
     end
 
-    def move_dirs
+    def moves
+        forward_steps + side_attacks
     end
 
     private 
 
     def at_start_row?
+        pos[0] == (color == :white) ? 6 : 1
     end
 
     def forward_dir
         # returns 1 or -1
+        color == :white ? -1 : 1
     end
 
     def forward_steps
+        i, j = pos
+        one_step = [i + forward_dir, j]
+        return [] unless board.valid_pos?(one_step) && board.empty?(one_step)
+
+        steps = [one_step]
+        two_step = [i + 2 * forward_dir, j]
+        steps << two_step if at_start_row? && board.empty?(two_step)
+        steps
     end
 
     def side_attacks
