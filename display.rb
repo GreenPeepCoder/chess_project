ee83ccad5608db72700e7cbd137a3e60a48ae809
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-21 15:51:03 -0500

    Add methods to the Display class that facilitate the rendering process

--- display.rb
+++ display.rb
@@ -1,13 +1,55 @@
 require 'colorize'
-require "cursor.rb"
+require_relative "cursor"
 require_relative "board"
 
 class Display
 
-    attr_accessor :board
+    attr_reader :board, :cursor
 
     def initialize(board)
         @board = board
         @cursor = Cursor.new([0, 0], board)
     end
+
+    def colors_for(i, j)
+        if cursor.cursor_pos == [i,j]
+            bg = :light_green
+        elsif (i+j).odd?
+            bg = :light_blue
+        else
+            bg = :light_yellow
+        end
+        {background: bg}
+    end
+
+    def build_grid
+        @board.rows.map.with_index do |row, idx|
+            build_row(row, idx)
+        end
+    end
+
+    def build_row(row, i)
+        row.map.with_index do |piece, j|
+            color_options = colors_for(i,j)
+            piece.to_s.colorize(color_options)
+        end
+    end
+
+    def render
+        system ("clear")
+        puts "Arrow keys, WASD, or vim to move, space or enter to confirm"
+        build_grid.each { |row| puts row.join}
+    end
+
+    def test
+        while true
+            self.render
+            @cursor.get_input
+        end
+    end
+
 end
+
+b = Board.new
+d = Display.new(b)
+d.test
