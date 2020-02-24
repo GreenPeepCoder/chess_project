509f3fec6160416f73ebed2876b29957fbf0e3a8
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-23 19:55:43 -0500

    Work on Game's #initialize and #play methods

--- game.rb
+++ game.rb
@@ -2,14 +2,34 @@ require_relative 'board'
 require_relative 'humanplayer'
 
 class Game
+    attr_reader :board, :display, :curr_player, :players
 
     def initialize
         @board = Board.new
         @display = Display.new(@board)
-
+        @players = {
+            white: HumanPlayer.new(:white, @display),
+            black: HumanPlayer.new(:black, @display)
+        }
+        @curr_player = :white
     end
 
     def play
+        until board.checkmate?(curr_player)
+            begin
+                start_pos, end_pos = players[curr_player].make_move(board)
+                board.move_piece(curr_player, start_pos, end_pos)
+
+                swap_turn!
+            rescue => exception
+                exception.message
+                retry
+            end
+        end
+
+        display.render
+        puts "#{curr_player} is in checkmate."
+        nil
     end
 
     private
@@ -18,5 +38,6 @@ class Game
     end
 
     def swap_turn!
+        @curr_player = curr_player == :white ? :black : :white
     end
 end
